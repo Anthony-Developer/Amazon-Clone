15bb8c721b19c3b1d90b95945345cc4600f57e5b
Anthony-Developer <AnthonyJimenez809@Gmail.com>
2020-11-09 21:12:21 -0500

    Started adding the header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAU58kDUG1ptz8sveE_rp9J30OabVK4AMU",
+    authDomain: "clone-a135f.firebaseapp.com",
+    databaseURL: "https://clone-a135f.firebaseio.com",
+    projectId: "clone-a135f",
+    storageBucket: "clone-a135f.appspot.com",
+    messagingSenderId: "237419910030",
+    appId: "1:237419910030:web:d66c9a538d8962a611cadf",
+    measurementId: "G-1ZYWRMVJN0"
+  };
