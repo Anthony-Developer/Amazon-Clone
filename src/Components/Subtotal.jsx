b47a35e82fcd02e2fb75bc2d6384f33d39a5dd69
Anthony-Developer <AnthonyJimenez809@Gmail.com>
2020-11-13 19:58:22 -0500

    The sum of products in the subtotal component

--- src/Components/Subtotal.jsx
+++ src/Components/Subtotal.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import '../Styling/Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from '../StateProvider'
+import { getBasketTotal } from '../Reducer'
 
 function Subtotal() {
+    // eslint-disable-next-line
+    const [{basket}, dispatch] = useStateValue()
+    console.log(basket)
+
     return (
         <div className="subtotal">
             <CurrencyFormat 
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={"text"}
                 thousandSeperator={true}
                 prefix={"$"}
                 renderText={(value) => (
                     <>
-                        <p>Subtotal (0 items):
-                            <strong> 0 </strong>
+                        <p>Subtotal ({basket.length} items):
+                            <strong> {value} </strong>
                         </p>
                         <small className="subtotal_gift">
                             <input type="checkbox" />
--- src/Reducer.js
+++ src/Reducer.js
@@ -2,8 +2,11 @@ export const initialState = {
     basket: [],
 }
 
+// Selector, adds all items to the total
+export const getBasketTotal = (basket) =>
+    basket?.reduce((amount, item) => item.price + amount, 0)
+
 const reducer = (state, action) => {
-    console.log(action)
     switch(action.type) {
         case 'ADD_TO_BASKET':
             return {
